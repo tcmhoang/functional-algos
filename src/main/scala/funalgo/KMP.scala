7305b435f76d718116639032a86de8260c0cf959
Cameron Hoang <tcmhoang@outlook.com>
2023-02-25 00:43:29 -0500

    Implement find method in KMP

--- src/main/scala/funalgo/KMP.scala
+++ src/main/scala/funalgo/KMP.scala
@@ -1,11 +1,30 @@
 package funalgo
 
+import java.util.stream.IntStream
+
 object KMP {
 
-  private def buildPrefixTable(pattern: String): Vector[Int] = ???
+  private def buildPrefixs(pattern: String): Vector[Int] = ???
+
+  private def auxFind(
+      prefixTable: Vector[Int],
+      pattern: String,
+      charwIdxes: List[Tuple2[Char, Int]],
+      sufffixIdx: Int
+  ): Option[Int] =
+    charwIdxes match
+      case (rune, idx) :: cdr =>
+        if pattern.charAt(sufffixIdx) == rune
+        then
+          val nSuffixIdx = sufffixIdx + 1
+          if nSuffixIdx == pattern.length() then Some(idx)
+          else auxFind(prefixTable, pattern, cdr, nSuffixIdx)
+        else if sufffixIdx == 0 then auxFind(prefixTable, pattern, cdr, 0)
+        else
+          auxFind(prefixTable, pattern, charwIdxes, prefixTable(sufffixIdx - 1))
+      case Nil => None
 
   def find(text: String, pattern: String): Option[Int] =
-    val prefixTable = buildPrefixTable(pattern)
-    (0 until text.length()).foldLeft(0)((prefixIdx, currIdx) => ???)
-    ???
+    auxFind(buildPrefixs(pattern), pattern, text.zipWithIndex.toList, 0)
+
 }
